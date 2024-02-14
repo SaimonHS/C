b3365472c2d8c1a3bbbddeb8f656d6d7750dcdfa
SaimonHS <saimonhasan.1436@gmail.com>
2024-02-14 10:18:21 +0600

    z

---
+++ Practiase/Reverse.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+main()
+{
+    char char1 = 'a', char2 = 'b', char3 = 'c';
+    printf("the reverse code of %c%c%c is: %c%c%c", char1, char2, char3, char3, char2, char1);
+    return (0);
+}
